b57d43e0422ad964d431ae2e2b6c3d35ce925a33
shidukimio <m4043098+github@gmail.com>
2022-08-26 22:56:07 +0900

    EslintのXOで半角スペース設定を有効に変更
    npm install --save-dev eslint-config-xo eslint-config-xo-react eslint-plugin-react eslint-plugin-react-hooks
    
    npm install --save-dev eslint-config-xo-space
    
    npm install --save-dev eslint-config-xo eslint-config-xo-typescript @typescript-eslint/parser @typescript-eslint/eslint-plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,41 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: [
-		'plugin:react/recommended',
-		'xo',
-	],
-	overrides: [
-		{
-			extends: [
-				'xo-typescript',
-			],
-			files: [
-				'*.ts',
-				'*.tsx',
-			],
-		},
-	],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: [
-		'react',
-	],
-	rules: {
-	},
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  // https://zenn.dev/toshiyuki/articles/d2d8b53a9cbfa8
+  extends: [
+    // https://zenn.dev/januswel/articles/402774d76424e71ac906
+    'react-app',
+    'react-app/jest',
+    'plugin:react/recommended',
+    'xo',
+    // https://github.com/xojs/eslint-config-xo-space
+    'xo-space',
+    // https://github.com/xojs/eslint-config-xo-react
+    'xo-react',
+    'xo-react/space',
+  ],
+  overrides: [
+    {
+      extends: [
+        // https://github.com/xojs/eslint-config-xo-typescript
+        'xo-typescript',
+        'xo-typescript/space',
+      ],
+      files: [
+        '*.ts',
+        '*.tsx',
+      ],
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
 };
