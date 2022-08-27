b54be81ed908388520b7da63fc37eb99d3716d75
shidukimio <m4043098+github@gmail.com>
2022-08-27 14:17:24 +0900

    Exampleの画像を設定

--- src/AppImageGallery.tsx
+++ src/AppImageGallery.tsx
@@ -1,7 +1,21 @@
 import type {ReactImageGalleryItem} from 'react-image-gallery';
 import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/css/image-gallery.css';
 
-const images: ReactImageGalleryItem[] = [];
+const images: ReactImageGalleryItem[] = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+];
 
 const AppImageGallery = () => (
   <ImageGallery items={images}/>
