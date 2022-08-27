bc46fd2195764c752b5493ab2b5a21025f361b18
shidukimio <m4043098+github@gmail.com>
2022-08-27 11:21:56 +0900

    ファイル全体でスラッシュ3つのESLintのチェックをOFF
    スラッシュ3つの定義をするファイルのため

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,3 +1,3 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
 // https://ginpen.com/2019/08/06/eslint-for-react-in-typescript/
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference types="react-scripts" />
