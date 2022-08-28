a65e96ce29460fd6d8e861e64881bbfd7c9d5766
shidukimio <m4043098+github@gmail.com>
2022-08-28 11:50:19 +0900

    storageから表示情報を取得する機能を追加

--- src/AppImageGallery.tsx
+++ src/AppImageGallery.tsx
@@ -1,24 +1,69 @@
+import {getDownloadURL, listAll, ref} from 'firebase/storage';
+import {useEffect, useState} from 'react';
 import type {ReactImageGalleryItem} from 'react-image-gallery';
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
+import storage from './firebase';
 
-const images: ReactImageGalleryItem[] = [
-  {
-    original: 'https://picsum.photos/id/1018/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1018/250/150/',
-  },
-  {
-    original: 'https://picsum.photos/id/1018/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1018/250/150/',
-  },
-  {
-    original: 'https://picsum.photos/id/1018/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1018/250/150/',
-  },
-];
+const imagesRef = ref(storage, 'image');
 
-const AppImageGallery = () => (
-  <ImageGallery items={images}/>
-);
+const AppImageGallery = () => {
+  const [stateImages, setStateImages] = useState<ReactImageGalleryItem[]>([]);
+
+  // StrictModeの影響で開発環境では2回表示処理が実行される
+  // https://www.ey-office.com/blog_archive/2021/06/30/i-found-out-why-the-component-is-drawn-twice-in-the-react/
+  // 空のuseSateで画面表示をしておいて、ダウンロードURLの取得が完了したら再表示を行う
+  useEffect(() => {
+    // 初期化済の時は取得処理をしない
+    if (stateImages.length > 0) {
+      return;
+    }
+
+    // 表示情報を取得
+    listAll(imagesRef).then(res => {
+      const itemSize = res.items.length;
+      console.log('count:' + String(itemSize));
+
+      // ダウンロードURLを取得
+      const images: ReactImageGalleryItem[] = [];
+      res.items.forEach(itemRef => {
+        console.log(itemRef.fullPath);
+        console.log('14');
+
+        // https://firebase.google.com/docs/storage/web/download-files
+        getDownloadURL(ref(storage, itemRef.fullPath)).then(url => {
+          console.log(url);
+
+          // 取得したダウンロードURLを貯める
+          console.log('21');
+          images.push({
+            original: url,
+            thumbnail: url,
+          });
+        }).catch(error => {
+          console.log(error);
+        }).finally(() => {
+          // ダウンロードURLを全て取得したか確認
+          console.log('count-download:' + String(images.length));
+          console.log('42');
+          if (itemSize === images.length) {
+            // 値を設定して画面再表示
+            console.log('44');
+            setStateImages(images);
+          }
+        });
+      });
+    }).catch(error => {
+      console.log(error);
+    }).finally(() => {
+      console.log('46');
+    });
+    console.log('33');
+  });
+
+  return (
+    <ImageGallery items={stateImages}/>
+  );
+};
 
 export default AppImageGallery;
