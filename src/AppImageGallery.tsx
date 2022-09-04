5afdd47c6c06e4e9aeb614a763e5a6261c958c80
shidukimio <m4043098+github@gmail.com>
2022-09-04 09:21:07 +0900

    リファクタリング
    データ取得処理を上位コンポーネントに移動

--- src/AppImageGallery.tsx
+++ src/AppImageGallery.tsx
@@ -1,124 +1,13 @@
 import { CircularProgress, Stack } from '@mui/material';
-import type { ListResult, StorageReference } from 'firebase/storage';
-import { getDownloadURL, getMetadata, listAll, ref } from 'firebase/storage';
-import type { Dispatch, SetStateAction } from 'react';
-import { useEffect, useState } from 'react';
 import type { ReactImageGalleryItem } from 'react-image-gallery';
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
-import storage from './firebase';
 
-const listRef = ref(storage, 'public/images');
-
-function setImages(
-  res: ListResult,
-  setStateImages: Dispatch<SetStateAction<ReactImageGalleryItem[]>>,
-) {
-  const itemSize = res.items.length;
-  console.log('count:' + String(itemSize));
-
-  // ダウンロードURLを取得
-  const images: ReactImageGalleryItem[] = [];
-  res.items.forEach((itemRef) => {
-    console.log(itemRef.fullPath);
-    console.log('14');
-    const imageRef: StorageReference = ref(storage, itemRef.fullPath);
-
-    // https://firebase.google.com/docs/storage/web/file-metadata?hl=ja
-    getMetadata(imageRef)
-      .then((metadata) => {
-        console.log(metadata);
-
-        // https://firebase.google.com/docs/storage/web/download-files
-        getDownloadURL(imageRef)
-          .then((url) => {
-            console.log(url);
-
-            // 取得したダウンロードURLを貯める
-            console.log('21');
-            images.push({
-              original: url,
-              thumbnail: process.env.PUBLIC_URL + '/thumbnail.svg',
-              description: metadata.updated,
-            });
-          })
-          .catch((error) => {
-            console.log(error);
-          })
-          .finally(() => {
-            // ダウンロードURLを全て取得したか確認
-            console.log('count-download:' + String(images.length));
-            console.log('42');
-            if (itemSize === images.length) {
-              // 並び替え
-              images.sort(
-                (
-                  imageA: ReactImageGalleryItem,
-                  imageB: ReactImageGalleryItem,
-                ) => {
-                  if (imageA.description === undefined) {
-                    return 0;
-                  }
-
-                  if (imageB.description === undefined) {
-                    return 0;
-                  }
-
-                  if (imageA.description < imageB.description) {
-                    return 1;
-                  }
-
-                  if (imageB.description < imageA.description) {
-                    return -1;
-                  }
-
-                  return 0;
-                },
-              );
-
-              // 値を設定して画面再表示
-              console.log('44');
-              setStateImages(images);
-            }
-          });
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
-        console.log('42:meta');
-      });
-  });
-}
-
-const AppImageGallery = () => {
-  const [stateImages, setStateImages] = useState<ReactImageGalleryItem[]>([]);
-
-  // StrictModeの影響で開発環境では2回表示処理が実行される
-  // https://www.ey-office.com/blog_archive/2021/06/30/i-found-out-why-the-component-is-drawn-twice-in-the-react/
-  // 空のuseSateで画面表示をしておいて、ダウンロードURLの取得が完了したら再表示を行う
-  useEffect(() => {
-    // 初期化済の時は取得処理をしない
-    console.log('18');
-    if (0 < stateImages.length) {
-      console.log('20');
-      return;
-    }
-
-    // 表示情報を取得
-    listAll(listRef)
-      .then((res) => {
-        setImages(res, setStateImages);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
-        console.log('46');
-      });
-    console.log('33');
-  });
-
+const AppImageGallery = ({
+  stateImages,
+}: {
+  stateImages: ReactImageGalleryItem[];
+}) => {
   // 表示情報取得前は待機のイメージを表示
   if (stateImages.length <= 0) {
     return (
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,7 @@
 import {
   AppBar,
   Box,
+  CircularProgress,
   Container,
   createTheme,
   CssBaseline,
@@ -12,6 +13,97 @@ import {
 import HomeIcon from '@mui/icons-material/Home';
 import AppImageGallery from './AppImageGallery';
 import { Copyright } from './Copyright';
+import storage from './firebase';
+import type { ListResult, StorageReference } from 'firebase/storage';
+import { getDownloadURL, getMetadata, listAll, ref } from 'firebase/storage';
+import type { Dispatch, SetStateAction } from 'react';
+import { Suspense } from 'react';
+import { useEffect, useState } from 'react';
+import type { ReactImageGalleryItem } from 'react-image-gallery';
+import { Stack } from '@mui/system';
+
+const listRef = ref(storage, 'public/images');
+
+function setImages(
+  res: ListResult,
+  setStateImages: Dispatch<SetStateAction<ReactImageGalleryItem[]>>,
+) {
+  const itemSize = res.items.length;
+  console.log('count:' + String(itemSize));
+
+  // ダウンロードURLを取得
+  const images: ReactImageGalleryItem[] = [];
+  res.items.forEach((itemRef) => {
+    console.log(itemRef.fullPath);
+    console.log('14');
+    const imageRef: StorageReference = ref(storage, itemRef.fullPath);
+
+    // https://firebase.google.com/docs/storage/web/file-metadata?hl=ja
+    getMetadata(imageRef)
+      .then((metadata) => {
+        console.log(metadata);
+
+        // https://firebase.google.com/docs/storage/web/download-files
+        getDownloadURL(imageRef)
+          .then((url) => {
+            console.log(url);
+
+            // 取得したダウンロードURLを貯める
+            console.log('21');
+            images.push({
+              original: url,
+              thumbnail: process.env.PUBLIC_URL + '/thumbnail.svg',
+              description: metadata.updated,
+            });
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+          .finally(() => {
+            // ダウンロードURLを全て取得したか確認
+            console.log('count-download:' + String(images.length));
+            console.log('42');
+            if (itemSize === images.length) {
+              // 並び替え
+              images.sort(
+                (
+                  imageA: ReactImageGalleryItem,
+                  imageB: ReactImageGalleryItem,
+                ) => {
+                  if (imageA.description === undefined) {
+                    return 0;
+                  }
+
+                  if (imageB.description === undefined) {
+                    return 0;
+                  }
+
+                  if (imageA.description < imageB.description) {
+                    return 1;
+                  }
+
+                  if (imageB.description < imageA.description) {
+                    return -1;
+                  }
+
+                  return 0;
+                },
+              );
+
+              // 値を設定して画面再表示
+              console.log('44');
+              setStateImages(images);
+            }
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        console.log('42:meta');
+      });
+  });
+}
 
 // https://mui.com/material-ui/customization/color/
 // https://material.io/resources/color/#!/?view.left=0&view.right=0&primary.color=3F51B5&secondary.color=FFEA00&primary.text.color=ECEFF1&secondary.text.color=3E2723
@@ -34,6 +126,33 @@ const theme = createTheme({
 
 // https://github.com/mui/material-ui/blob/v5.10.2/docs/data/material/getting-started/templates/album/Album.tsx
 export const Home = () => {
+  const [stateImages, setStateImages] = useState<ReactImageGalleryItem[]>([]);
+
+  // StrictModeの影響で開発環境では2回表示処理が実行される
+  // https://www.ey-office.com/blog_archive/2021/06/30/i-found-out-why-the-component-is-drawn-twice-in-the-react/
+  // 空のuseSateで画面表示をしておいて、ダウンロードURLの取得が完了したら再表示を行う
+  useEffect(() => {
+    // 初期化済の時は取得処理をしない
+    console.log('18');
+    if (0 < stateImages.length) {
+      console.log('20');
+      return;
+    }
+
+    // 表示情報を取得
+    listAll(listRef)
+      .then((res) => {
+        setImages(res, setStateImages);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        console.log('46');
+      });
+    console.log('33');
+  });
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -49,7 +168,15 @@ export const Home = () => {
         <Container sx={{ py: 1 }} maxWidth='lg'>
           <Grid container>
             <Grid item xs={12}>
-              <AppImageGallery />
+              <Suspense
+                fallback={
+                  <Stack alignItems='center'>
+                    <CircularProgress />
+                  </Stack>
+                }
+              >
+                <AppImageGallery stateImages={stateImages} />
+              </Suspense>
             </Grid>
           </Grid>
         </Container>
